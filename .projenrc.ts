79d431e17ed5b15b54c920f0efea6f481e83b97d
Thorsten Hoeger <thorsten.hoeger@taimos.de>
2025-11-17 00:08:14 +0100

    fix: release

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,15 +1,17 @@
 import { awscdk, javascript } from 'projen';
+import { ReleaseTrigger } from 'projen/lib/release';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Thorsten Hoeger',
   authorAddress: 'thorsten.hoeger@taimos.de',
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.222.0',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.9.0',
+  constructsVersion: '10.4.2',
   name: 'cdk-devops',
   packageManager: javascript.NodePackageManager.NPM,
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/hoegertn/cdk-devops.git',
-
+  repositoryUrl: 'https://github.com/open-constructs/cdk-devops.git',
+  releaseTrigger: ReleaseTrigger.manual(),
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="cdk-devops.Hello"></a>
+
+#### Initializers <a name="Initializers" id="cdk-devops.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from 'cdk-devops'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#cdk-devops.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="cdk-devops.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
